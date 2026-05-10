42e73c816c774164adb7c52ab2c8e343cf1e79ba
saeedelsayed <saidelsayed922@gmail.com>
2026-05-10 13:26:40 +0200

    importing 3D spaceship model

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ModelViewer from './components/modelViewer';
+
+const App = () => {
+    return (
+        <main>
+        <ModelViewer/>
+        </main>
+    )
+}
+export default App
---
+++ src/components/modelViewer.jsx
@@ -0,0 +1,18 @@
+import {Canvas} from "@react-three/fiber"
+import Optimized from "./models/Optimized.jsx"
+import {OrbitControls} from "@react-three/drei";
+
+const ModelViewer = () => {
+    return (
+        <section className="w-screen h-screen flex justify-center items-center bg-[#222] overflow-hidden">
+            <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }} >
+            <ambientLight intensity={0.7}/>
+                <directionalLight position={[5, 5, 5]} intensity={2}/>
+            <Optimized scale={1}  position={[0,0,0]}/>
+            <OrbitControls enableZoom={true} />
+            </Canvas>
+        </section>
+    )
+}
+
+export default ModelViewer;
